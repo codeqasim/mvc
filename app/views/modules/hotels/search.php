47e92fd02dd0f31db2bfb183d7aba331d06352c7
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-06-17 22:27:52 +0500

    Update search.php

--- app/views/modules/hotels/search.php
+++ app/views/modules/hotels/search.php
@@ -22,14 +22,14 @@
                         <div class="c3 c-sm-6">
                             <span class="input-label"><i class="mdi mdi-calendar"></i> Checkin</span>
                             <div class="input-items">
-                                <input name="checkin" class="checkin" id="checkin" type="text" placeholder="" value="20-06-2020"  />
+                                <input name="checkin" class="checkin" id="checkin" type="text" placeholder="" value="20-06-2020" readonly="readonly"/>
                             </div>
                         </div>
                         <div class="c3 c-sm-6">
                             <span class="input-label"><i class="mdi mdi-calendar"></i> Checkout</span>
                             <div class="input-items">
                                 <span class="dashed hide show-md">-</span>
-                                <input name="checkout" class="checkout" id="checkout" type="text" placeholder="" value="22-06-2020"  />
+                                <input name="checkout" class="checkout" id="checkout" type="text" placeholder="" value="22-06-2020" readonly="readonly" />
                             </div>
                         </div>
                         <div class="c6 row-rtl pt-15">

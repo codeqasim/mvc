967aa9c3f08c8ee428d1c351bc6b0f0860128e65
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-06-18 18:05:16 +0500

    i

--- app/config.php
+++ app/config.php
@@ -10,7 +10,8 @@
 $root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
 $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 
-// translation lib
+// included libs
+require 'lib/compress/compress.php';
 require 'lib/i18n/i18n.class.php';
 $i18n = new i18n('app/lang/{LANGUAGE}.json', 'app/lib/i18n/langcache/', 'en');
 

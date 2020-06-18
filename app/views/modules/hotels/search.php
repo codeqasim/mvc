9a8152600fe382788fa60543144d05c7ebd44a5e
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-06-19 00:02:49 +0500

    Update search.php

--- app/views/modules/hotels/search.php
+++ app/views/modules/hotels/search.php
@@ -7,7 +7,7 @@
                     <span class="input-label"><i class="mdi mdi-hotel"></i> City Name</span>
                     <div class="input-items">
                         <select id="city" name="city" class="form-control" required>
-                        <option value="<?=city?><?=country?>"> <?=city?>, <?=country?></option>
+                        <option value="<?=city?>"> <?=city?>, <?=country?></option>
                         </select>
                        <!--<input autocomplete="off" type="search" placeholder="city" id="city" name="city" value="lahore" class="city"/>-->
                     </div>

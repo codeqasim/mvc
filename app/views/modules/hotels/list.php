6b488ec5f903c1d224b202bc55b0a17146b1513f
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-06-19 04:11:56 +0500

    i

--- app/views/modules/hotels/list.php
+++ app/views/modules/hotels/list.php
@@ -74,6 +74,41 @@
             <input type="hidden" name="max-value" value="">
           </div>
 
+          <script>
+          // Set visual min and max values and also update value hidden form inputs
+            $(document).ready(function() {
+              $('.noUi-handle').on('click', function() {
+                $(this).width(50);
+              });
+              var rangeSlider = document.getElementById('slider-range');
+              var moneyFormat = wNumb({
+                decimals: 0,
+                thousand: ',',
+                prefix: 'USD '
+              });
+              noUiSlider.create(rangeSlider, {
+                start: [450, 1500],
+                step: 1,
+                range: {
+                  'min': [20],
+                  'max': [2000]
+                },
+                format: moneyFormat,
+                connect: true
+              });
+
+              // Set visual min and max values and also update value hidden form inputs
+              rangeSlider.noUiSlider.on('update', function(values, handle) {
+                document.getElementById('slider-range-value1').innerHTML = values[0];
+                document.getElementById('slider-range-value2').innerHTML = values[1];
+                document.getElementsByName('min-value').value = moneyFormat.from(
+                  values[0]);
+                document.getElementsByName('max-value').value = moneyFormat.from(
+                  values[1]);
+              });
+            });
+          </script>
+
         </div>
         <div class="filter-section star-rating mt-30 pb-10">
           <div class="filter-header">
--- assets/js/price_ranger.js
+++ assets/js/price_ranger.js
@@ -1,38 +1,3 @@
- // Set visual min and max values and also update value hidden form inputs 
-$(document).ready(function() {
-  $('.noUi-handle').on('click', function() {
-    $(this).width(50);
-  });
-  var rangeSlider = document.getElementById('slider-range');
-  var moneyFormat = wNumb({
-    decimals: 0,
-    thousand: ',',
-    prefix: 'USD '
-  });
-  noUiSlider.create(rangeSlider, {
-    start: [450, 1500],
-    step: 1,
-    range: {
-      'min': [20],
-      'max': [2000]
-    },
-    format: moneyFormat,
-    connect: true
-  });
-
-  // Set visual min and max values and also update value hidden form inputs
-  rangeSlider.noUiSlider.on('update', function(values, handle) {
-    document.getElementById('slider-range-value1').innerHTML = values[0];
-    document.getElementById('slider-range-value2').innerHTML = values[1];
-    document.getElementsByName('min-value').value = moneyFormat.from(
-      values[0]);
-    document.getElementsByName('max-value').value = moneyFormat.from(
-      values[1]);
-  });
-});
-
-
-
 // https://refreshless.com/nouislider/
 /*! nouislider - 8.3.0 - 2016-02-14 17:37:19 */
 (function(factory) {

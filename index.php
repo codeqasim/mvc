cd2e32c5ff89acb0ba5b7cc6ee781268854dd6f7
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-06-18 17:54:41 +0500

    i

--- index.php
+++ index.php
@@ -8,6 +8,35 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
+    if (!file_exists(".htaccess")) {
+    echo '<!DOCTYPE html><html lang="en">';
+    echo "<style>body{font-family:calibri;padding:0px;margin:25px}.alert{background:red}.box{height:24px;width:10%;position:relative:display:block}code{font-family:monospace; color: crimson; background-color: #f1f1f1; padding: 0px; font-size: 16px; }</style>";
+    echo "The .htaccess file does not exist on main directory of your site.<br>";
+    echo "If you can't see this file, you can create a new file named \".htaccess\" in the main directory of your site and you can add these codes to inside this file:<br>";
+    echo "<br>";
+    echo "<code>";
+    echo "RewriteEngine On<br>";
+    echo "RewriteCond %{REQUEST_FILENAME} !-f<br>";
+    echo "RewriteCond %{REQUEST_FILENAME} !-d<br>";
+    echo "RewriteRule ^(.*)$ index.php?/$1 [L]";
+    echo "</code>";
+    echo "<br><br><br>";
+    echo "<strong>If you have SSL installed use this code instead</strong><br><br>";
+    echo "<code>";
+    echo "RewriteEngine on<br>";
+    echo "DirectoryIndex load.php index.php<br>";
+    echo "RewriteCond %{HTTPS} !=on<br>";
+    echo "RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301,NE]<br>";
+    echo "RewriteCond %{SCRIPT_FILENAME} !-d<br>";
+    echo "RewriteCond %{SCRIPT_FILENAME} !-f<br>";
+    echo "RRewriteRule ^(.*)$ index.php?url=$1 [QSA,L]<br>";
+    echo "</code>";
+    exit();
+    }
+    
+    $php_version = explode('.', phpversion());
+    if ($php_version[0] < 7) { include_once './assets/fix/php.php'; die(); }
+
     $root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
     $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 

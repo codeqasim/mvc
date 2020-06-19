a167d46bb53e2e1741f9273aee590a5fd31a9f75
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-06-20 03:10:23 +0500

    Update footer.php

--- app/views/footer.php
+++ app/views/footer.php
@@ -89,12 +89,7 @@
 
 <!-- layload -->
 <script>
-$(function() { $('.lazy').lazy({ effect: "fadeIn", effectTime: 200, threshold: 0,
-
-    placeholder : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
-
-
- }); });
+$(function() { $('.lazy').lazy({ effect: "fadeIn", effectTime: 200, threshold: 0, }); });
 </script>
 
 <!-- select2 init -->

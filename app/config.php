dbccc521a5a0d80fa0f9566bd62cc26a270485bc
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-06-21 05:22:34 +0500

    Update config.php

--- app/config.php
+++ app/config.php
@@ -64,16 +64,13 @@ $ip = call_user_func(function(){
 });
 
 
-
+// geo api 
 $geo_url = "http://api.ipstack.com/";
 $geo = $geo_url.$ip."?access_key=7f51f6157497018e41465e3d87e86f60";
-// echo $geo;
-
-//$geo_url = "http://ip-api.com/json/";
 $details = json_decode(file_get_contents($geo));
+//$geo_url = "http://ip-api.com/json/";
 // $geo_url = "https://api.ipdata.co/?api-key=test/";
 // $geo_url = "https://geolocation-db.com/jsonp";
-
 define('city', $details->city);
 define('country', $details->country_name);
 

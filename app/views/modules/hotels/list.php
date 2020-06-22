67a360885e165e7cca8806f6635a921b70adda4e
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-06-23 04:36:37 +0500

    i

--- app/views/modules/hotels/list.php
+++ app/views/modules/hotels/list.php
@@ -118,40 +118,53 @@
 
                 <div class="filters filter-section">
 
-                <label for="s5"> <input type="radio" data-filter="s5" id="s5" name="stars" value="s5" class="filter-stars"/>
+                <label class="btn" for="0" data-filter=""> <input type="radio" id="0" name="stars"/>
+                All
+                <strong>
+                <?php  if (!empty($totalhotel)){echo $totalhotel; }else{echo $totalempty;}?>
+                </strong>
+                </label>
+
+                <label class="btn" for="5" data-filter=".s5"> <input type="radio" id="5" name="stars"/>
                 <span class="stars star5"></span>
                 <strong>
                 <?php if (empty($listempty)) { echo $five_stars; }else{echo'0';} ?>
                 </strong>
                 </label>
+
                 <div class="clear"></div>
-                <label for="s4"> <input type="radio" data-filter="s4" id="s4" name="stars" value="s4" class="filter-stars"/>
+                <label class="btn" for="4" data-filter=".s4"> <input type="radio" id="4" name="stars"/>
                 <span class="stars star4"></span>
                 <strong>
                 <?php if (empty($listempty)) { echo $four_stars; }else{echo'0';} ?>
                 </strong>
                 </label>
+
                 <div class="clear"></div>
-                <label for="s3"> <input type="radio" data-filter="s3" id="s3" name="stars" value="s3" class="filter-stars"/>
+                <label class="btn" for="3" data-filter=".s3"> <input type="radio" id="3" name="stars"/>
                 <span class="stars star3"></span>
                 <strong>
                 <?php if (empty($listempty)) { echo $three_stars; }else{echo'0';} ?>
                 </strong>
                 </label>
+
                 <div class="clear"></div>
-                <label for="s2"> <input type="radio" data-filter="s2" id="s2" name="stars" value="s2" class="filter-stars"/>
+                <label class="btn" for="2" data-filter=".s2"> <input type="radio" id="2" name="stars"/>
                 <span class="stars star2"></span>
                 <strong>
                 <?php if (empty($listempty)) { echo $two_stars; }else{echo'0';} ?>
                 </strong>
                 </label>
+
                 <div class="clear"></div>
-                <label for="s1"> <input type="radio" data-filter="s1" id="s1" name="stars" value="s1" class="filter-stars"/>
+                <label class="btn" for="1" data-filter=".s1"> <input type="radio" id="1" name="stars"/>
                 <span class="stars star1"></span>
                 <strong>
                 <?php if (empty($listempty)) { echo $one_stars; }else{echo'0';} ?>
                 </strong>
                 </label>
+
+
                 <div class="clear"></div>
               </div>
               </div>
@@ -244,15 +257,7 @@
 
          <div class="filters filter-section">
         <div class="row">
-            <div class="col-sm-4">
-                <span class="filter-label">star:</span>
-                <div class="btn-group" role="group" data-filter-group="color">
-                    <span class="btn btn-sm btn-default btn-filter is-checked" data-filter="">Any</span>
-                    <span class="btn btn-sm btn-default btn-filter" data-filter=".s1">s1</span>
-                    <span class="btn btn-sm btn-default btn-filter" data-filter=".s2">s2</span>
-                    <span class="btn btn-sm btn-default btn-filter" data-filter=".s3">s3</span>
-                </div>
-            </div>
+
 
             <div class="col-sm-4">
                 <span class="filter-label">price:</span>
@@ -527,168 +532,7 @@ function concatValues( obj ) {
 <script src='<?=root.js;?>handlebars.min.js'></script>
 <?=$hotels;?>
 
-<script>
-  var itemSelector = ".item";
-  var $stars = $('.filter-stars');
-  var $price = $('.filter-price');
-  var $container = $('#hotels').isotope({ itemSelector: itemSelector });
 
-  //Ascending order
-  var responsiveIsotope = [ [480, 4] , [720, 6] ];
-  var itemsPerPageDefault = 15;
-  var itemsPerPage = defineItemsPerPage();
-  var currentNumberPages = 1;
-  var currentPage = 1;
-  var currentFilter = '*';
-  var filterAttribute = 'data-filter';
-  var filterValue = "";
-  var pageAttribute = 'data-page';
-  var pagerClass = 'isotope-pager';
-
-  // update items based on current filters
-  function changeFilter(selector) { $container.isotope({ filter: selector }); }
-
-  //grab all checked filters and goto page on fresh isotope output
-  function goToPage(n) {
-    currentPage = n;
-    var selector = itemSelector;
-    var exclusives = [];
-        // for each box checked, add its value and push to array
-        $stars.each(function (i, elem) {
-            if (elem.checked) {
-                selector += ( currentFilter != '*' ) ? '.'+elem.value : '';
-                exclusives.push(selector);
-            }
-        });
-        // smash all values back together for 'and' filtering
-        filterValue = exclusives.length ? exclusives.join('') : '*';
-
-        // add page number to the string of filters
-        var wordPage = currentPage.toString();
-        filterValue += ('.'+wordPage);
-
-    changeFilter(filterValue);
-  }
-
-  // determine page breaks based on window width and preset values
-  function defineItemsPerPage() {
-    var pages = itemsPerPageDefault;
-
-    for( var i = 0; i < responsiveIsotope.length; i++ ) {
-        if( $(window).width() <= responsiveIsotope[i][0] ) {
-            pages = responsiveIsotope[i][1];
-            break;
-        }
-    }
-    return pages;
-  }
-
-  function setPagination() {
-
-    var SettingsPagesOnItems = function(){
-        var itemsLength = $container.children(itemSelector).length;
-        var pages = Math.ceil(itemsLength / itemsPerPage);
-        var item = 1;
-        var page = 1;
-        var selector = itemSelector;
-        var exclusives = [];
-            // for each box checked, add its value and push to array
-            $stars.each(function (i, elem) {
-                if (elem.checked) {
-                    selector += ( currentFilter != '*' ) ? '.'+elem.value : '';
-                    exclusives.push(selector);
-                }
-            });
-            // smash all values back together for 'and' filtering
-            filterValue = exclusives.length ? exclusives.join('') : '*';
-            // find each child element with current filter values
-            $container.children(filterValue).each(function(){
-                // increment page if a new one is needed
-                if( item > itemsPerPage ) {
-                    page++;
-                    item = 1;
-                }
-                // add page number to element as a class
-                wordPage = page.toString();
-
-                var classes = $(this).attr('class').split(' ');
-                var lastClass = classes[classes.length-1];
-                // last class shorter than 4 will be a page number, if so, grab and replace
-                if(lastClass.length < 4){
-                    $(this).removeClass();
-                    classes.pop();
-                    classes.push(wordPage);
-                    classes = classes.join(' ');
-                    $(this).addClass(classes);
-                } else {
-                    // if there was no page number, add it
-                   $(this).addClass(wordPage);
-                }
-                item++;
-            });
-        currentNumberPages = page;
-    }();
-
-    // create page number navigation
-    var CreatePagers = function() {
-
-        var $isotopePager = ( $('.'+pagerClass).length == 0 ) ? $('<div class="'+pagerClass+'"></div>') : $('.'+pagerClass);
-
-        $isotopePager.html('');
-        if(currentNumberPages > 1){
-          for( var i = 0; i < currentNumberPages; i++ ) {
-              var $pager = $('<a href="javascript:void(0);" class="pager" '+pageAttribute+'="'+(i+1)+'"></a>');
-                  $pager.html(i+1);
-
-                  $pager.click(function(){
-                      var page = $(this).eq(0).attr(pageAttribute);
-                      goToPage(page);
-                  });
-              $pager.appendTo($isotopePager);
-          }
-        }
-        $container.after($isotopePager);
-    }();
-  }
-  // remove checks from all boxes and refilter
-  function clearAll(){
-    $stars.each(function (i, elem) {
-        if (elem.checked) {
-            elem.checked = null;
-        }
-    });
-   currentFilter = '*';
-   setPagination();
-   goToPage(1);
-  }
-
-  setPagination();
-  goToPage(1);
-
-  //event handlers
-  $stars.change(function(){
-    var filter = $(this).attr(filterAttribute);
-    currentFilter = filter;
-    setPagination();
-    goToPage(1);
-  });
-
-  $('#clear-filters').click(function(){clearAll()});
-
-  $(window).resize(function(){
-    itemsPerPage = defineItemsPerPage();
-    setPagination();
-    goToPage(1);
-  });
-
-</script>
-<script>
-  $(".filter-item,.pager").click(function() {
-  $('html, body').animate({
-  scrollTop: $("#listing").offset().top
-  }, 1);
-  });
-</script>
 
 <script>
 $(document).ready(function(){
@@ -699,5 +543,13 @@ $(document).ready(function(){
     });
   });
 });
+</script>
+
+<script>
+  $(".filter-item,.pager").click(function() {
+  $('html, body').animate({
+  scrollTop: $("#listing").offset().top
+  }, 1);
+  });
 </script>
 

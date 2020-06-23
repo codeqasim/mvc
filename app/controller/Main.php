714781574bc2928eb06ef3bde2c93dbdf888d2f7
Younas22 <55877886+Younas22@users.noreply.github.com>
2020-06-23 17:39:01 +0500

    update

--- app/controller/Main.php
+++ app/controller/Main.php
@@ -54,12 +54,22 @@ class Main
         echo '<script>window.location.href = "'.$test.'";</script>';
         }else
         {
-        $test = $_POST['test'];
+        // $test = $_POST['test'];
         $base_url = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+        if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+        $link = "https";
+        else
+        $link = "http";
+        // Here append the common URL characters.
+        $link .= "://";
+        // Append the host(domain name, ip) to the URL.
+        $link .= $_SERVER['HTTP_HOST'];
+        // Append the requested resource location to the URL
+        $link .= $_SERVER['REQUEST_URI'];
         $base_explode = explode('/', $base_url);
         $lang_val = $base_explode[2];
         $_SESSION['session_lang'] = $lang_val;
-        echo '<script>window.location.href = "'.$test.'";</script>';
+        echo '<script>window.location.href = "'.$link.'";</script>';
     }
 }
 
--- app/views/menu.php
+++ app/views/menu.php
@@ -3,16 +3,12 @@ if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
     $link = "https";
 else
     $link = "http";
-
 // Here append the common URL characters.
 $link .= "://";
- 
 // Append the host(domain name, ip) to the URL.
 $link .= $_SERVER['HTTP_HOST'];
-
 // Append the requested resource location to the URL
 $link .= $_SERVER['REQUEST_URI'];
-
 $base_url = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
 $base_explode = explode('/', $base_url);
 ?>
